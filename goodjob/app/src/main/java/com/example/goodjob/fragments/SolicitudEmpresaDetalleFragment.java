46ee76b893fa500c5a62feddb906055491080159
gustavo <lizarzaburumercado@gmail.com>
2019-12-12 15:51:06 -0500

    SolicitudEmpresaDetalle-registrando-en-la-clase

--- goodjob/app/src/main/java/com/example/goodjob/fragments/SolicitudEmpresaDetalleFragment.java
+++ goodjob/app/src/main/java/com/example/goodjob/fragments/SolicitudEmpresaDetalleFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.goodjob.R;
@@ -17,6 +19,32 @@ public class SolicitudEmpresaDetalleFragment extends Fragment {
     /* TODO 1.1 aqui declaras los views del layout.
         Ejemplo: private TextView fechaRegistro;
     * */
+    TextView razon;
+    TextView razonSocial;
+
+    TextView nom;
+    TextView nombreComercial;
+
+    TextView ru;
+    TextView ruc;
+
+    TextView telf;
+    TextView telefono;
+
+    TextView corr;
+    TextView correo;
+
+    TextView codigo;
+    TextView codigoPostal;
+
+    TextView distri;
+    TextView distrito;
+
+    TextView direcc;
+    TextView direccion;
+
+    ImageButton aceptarSolicitud;
+    ImageButton rechazarSolicitud;
 
     public SolicitudEmpresaDetalleFragment() {
     }
@@ -38,12 +66,50 @@ public class SolicitudEmpresaDetalleFragment extends Fragment {
         /* TODO 1.2 aqui haces los findViewById();
             Ejemplo: fechaRegistro = v.findViewById(R.id.tvFechaRegistro);
         * */
+        razon = v.findViewById(R.id.tvRazon);
+        razonSocial = v.findViewById(R.id.tvRazonSocial);
+
+        nom = v.findViewById(R.id.tvNombre);
+        nombreComercial = v.findViewById(R.id.tvNombreComercial);
+
+        ru = v.findViewById(R.id.tvRu);
+        ruc = v.findViewById(R.id.tvRuc);
+
+        telf = v.findViewById(R.id.tvTelef);
+        telefono = v.findViewById(R.id.tvTelefono);
+
+        corr = v.findViewById(R.id.tvCorr);
+        correo = v.findViewById(R.id.tvCorreo);
+
+        codigo = v.findViewById(R.id.tvCodigo);
+        codigoPostal = v.findViewById(R.id.tvCodigoPostal);
+
+        distri = v.findViewById(R.id.tvDistri);
+        distrito = v.findViewById(R.id.tvDistrito);
+
+        direcc= v.findViewById(R.id.tvDirecc);
+        direccion = v.findViewById(R.id.tvDireccion);
+
+        aceptarSolicitud =v.findViewById(R.id.btnAceptarSolicitud);
+        rechazarSolicitud = v.findViewById(R.id.btnRechazarSolicitud);
+
+
     }
 
     private void mostrarDatos(Empresa e) {
         /* TODO 1.3 y aca haces los setText para mostrar la info en el layout.
             Ejemplo: fechaRegistro.setText(e.getFechaRegistro());
         * */
+
+        razonSocial.setText(e.getRazonSocial());
+        telefono.setText(e.getRazonSocial());
+        ruc.setText(e.getRuc());
+        telefono.setText(e.getTelefono());
+        correo.setText(e.getCorreo());
+        codigoPostal.setText(e.getCodigoPostal());
+        distrito.setText(e.getDistrito());
+        direccion.setText(e.getDireccion());
+
     }
 
     /* TODO 1.4 este solo es un recordatorio para que borres todos los TODO(1.0 - 1.4)
